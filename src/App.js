31989e320cc5bde8cd849ee507c8c3752a4c1e04
Brandon Raphael <brandonraphael1@gmail.com>
2017-02-27 21:15:31 -0700

    added expand/collapse button to source list

--- src/App.js
+++ src/App.js
@@ -36,11 +36,8 @@ class App extends Component {
           <Route path='/hacker-news' component={Newspage} source="hacker-news"/>
           <Route path='/ign' component={Newspage} source="ign"/>
           <Route path='/independent' component={Newspage} source="independent"/>
-          <Route path='/mashable' component={Newspage} source="mashable"/>
-          <Route path='/metro' component={Newspage} source="metro"/>
-          <Route path='/mirror' component={Newspage} source="mirror"/>
-          <Route path='/mtv-news' component={Newspage} source="mtv-news"/>
-          <Route path='/mtv-news-uk' component={Newspage} source="mtv-news-uk"/>
+          <Route path='/masha' component={Newspage} source=""/>
+
           <Route path='/the-guardian-uk' component={Newspage} source="the-guardian-uk"/>
           <Route path='/the-new-york-times' component={Newspage} source="the-new-york-times"/>
           <Route path='/the-wall-street-journal' component={Newspage} source="the-wall-street-journal"/>
