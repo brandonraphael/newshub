8dd026a46b7361b8e9b83641e4519b6608839c09
Brandon Raphael <brandonraphael1@gmail.com>
2017-03-01 12:33:19 -0700

    moved source list box

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Router history={browserHistory}>
           <Route path='/' component={Homepage} />
           <Route path='/abc-news-au' component={Newspage} source="abc-news-au"/>
